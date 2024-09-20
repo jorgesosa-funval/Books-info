7e9c6af7b61b6eab6f0fe66e41d716525ee6c9d9
Jorge Sosa <jorgesosa@fundaciondevalores.org>
2024-09-20 08:56:39 -0400

    update some styles

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-async function fetchData(url) {
-    const data = await fetch(url);
-    const json = await data.json();
+import BooksController from "./Controller/BooksController.js";
+BooksController.bookList()
 
-    return json;
-}
+const filerValue = document.querySelector('#filerValue')
+const oflabel = document.querySelector('#offcanvasExampleLabel');
 
-export default fetchData;
+
+
+filerValue.addEventListener('click', (e) => {
+    oflabel.textContent = e.target.textContent
+
+    if (e.target.textContent.toLowerCase() === 'categories') {
+        BooksController.categoriesList()
+    } else if (e.target.textContent.toLowerCase() === 'authors') {
+        BooksController.authorsList()
+    } else {
+        BooksController.bookList()
+    }
+
+})
